0751f7119b0273d815ca61c301576e8a0b72d8cc
Lyra-B <glykeriapeppa@gmail.com>
2015-03-30 18:00:23 +0100

    Created calculatePositions Function

--- app/assets/javascripts/fruits.js
+++ app/assets/javascripts/fruits.js
@@ -5,3 +5,17 @@
 // 2. calculatePositions
 
 // 3. Set positions on the box
+
+function calculatePositions(element){
+  var expectedData = [];
+  for(var i = 0; i < element.children.length; i++){
+    element.children[i].setAttribute("data-position", i );
+    var ExpId = element.children[i].id
+    var ExpPos = element.children[i].dataset.position
+    expectedData.push({ id: parseInt(ExpId), position: parseInt(ExpPos)})
+  }
+  return expectedData;
+}
+
+
+
